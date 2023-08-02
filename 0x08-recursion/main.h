605eb22f34244ac4cf82fafd827bd9f505f9e688
meryemjamali <meryem2jamali@gmail.com>
2023-08-02 09:47:14 -0700

    puts recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int sqr_rt(int n, int sqrrtnum);
+int is_prime_number(int n);
+int prime_aid (int n, int aid);
+int is_palindrome(char *s);
+int get_str_lenght(char *s);
+int paliandrome_aid (char *s, int a, int z);
+int wildcmp(char *s1, char *s2);
+#endif
