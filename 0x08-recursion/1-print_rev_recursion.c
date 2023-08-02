c081071f094263ce2ada31ab0c7b4f4d21a617c2
meryemjamali <meryem2jamali@gmail.com>
2023-08-02 09:52:13 -0700

    print reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include <stdio.h>
+/*by div-styl*/
+/**
+ * _print_rev_recursion -reverse string using recursion
+ * @s: is the string that should be reversed
+ * Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
