e3aaa1eef6af3c6b6df09d41ef24ccedbaf8bf53
meryemjamali <meryem2jamali@gmail.com>
2023-08-04 09:50:13 -0700

    libmy

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _strcat -This function combine two strings;
+* @dest: chart
+* @src: chart
+* Return: a pointer to the resulting string des
+*/
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
